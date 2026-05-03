2a97f57bcf7d5e840ff0fab3ba010c0b63bd961b
ratiisshm-crypto <ratiisshm@gmail.com>
2026-05-03 14:52:51 +0530

    Add files via upload

---
+++ MatrixMultiplication.py
@@ -0,0 +1,15 @@
+x = [[8, 2],
+     [4, 1]]
+
+y = [[3, 8],
+     [9, 15]]
+
+answer = [[0,0],[0,0]]
+
+for i in range(len(x)):
+    for j in range(len(y[0])):
+        for k in range(len(y)):
+            answer[i][j] += x[i][k] * y[k][j]
+
+for r in answer:
+    print(r)
