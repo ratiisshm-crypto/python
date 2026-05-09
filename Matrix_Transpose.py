b161ebe204edbefe1c8d2106ea4b7d7d762f16e1
ratiisshm-crypto <ratiisshm@gmail.com>
2026-05-09 18:43:51 +0530

    Add files via upload

---
+++ Matrix_Transpose.py
@@ -0,0 +1,15 @@
+x = [[8, 2, 3],
+     [4, 1, 9],
+     [1, 4, 8]]
+
+answer = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
+
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        answer[i][j] = x[j][i]
+
+for r in answer:
+    print(r)
+     
