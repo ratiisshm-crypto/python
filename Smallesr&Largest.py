6ffbe9ad305ab2c791ba5c0cfbe51380153c0004
ratiisshm-crypto <ratiisshm@gmail.com>
2026-06-21 17:54:53 +0530

    Add files via upload

---
+++ Smallesr&Largest.py
@@ -0,0 +1,26 @@
+def smallest_largest_words(str1):
+    word = ""
+    all_words = []
+    str1 = str1 + " "
+
+    for i in range(0, len(str1)):
+        if str1[i] != ' ':
+            word = word + str1[i]
+        else:
+            if word != "":
+                all_words.append(word)
+            word = ""
+    small = large = all_words[0]
+
+    for k in range(0, len(all_words)):
+        if len(small) > len(all_words[k]):
+            small = all_words[k]
+        if len(large) < len(all_words[k]):
+            large = all_words[k]
+
+    return small, large
+
+s = input("Enter a sentence: ")
+small, large = smallest_largest_words(s)
+print("Smallest word: ", small)
+print("Largest word:", large)
