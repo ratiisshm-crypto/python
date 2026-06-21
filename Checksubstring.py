ffe19b94d2d7ba7b4eadb90f1be56388c7152a89
ratiisshm-crypto <ratiisshm@gmail.com>
2026-06-21 18:04:08 +0530

    Add files via upload

---
+++ Checksubstring.py
@@ -0,0 +1,13 @@
+def isSubstring(s1, s2):
+    if s1 in s2:
+        return s2.index(s1)
+    return -1
+
+if __name__ == "__main__":
+    s1 = "Welcome"
+    s2 = "welcome to codingal"
+    result = isSubstring(s1, s2)
+    if result == -1:
+        print("Not present")
+    else:
+        print("Present at index " + str(result))
